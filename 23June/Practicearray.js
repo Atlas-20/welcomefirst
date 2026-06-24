47b872fac314eb0f8670c97ce5a6c5f2ececdf66
Atlas <atlastoo012@gmail.com>
2026-06-24 17:12:56 +0530

    Well this is the second commit just trying to learn git

---
+++ 23June/Practicearray.js
@@ -0,0 +1,8 @@
+//Problem: Create an array containing diferent names of scientist
+//Problem: Add "schrodinger" to it 
+//Remove any scientist from the middle of the list 
+//filter the list to only include those are harmful to the civilization 
+//sort the list of the scientist in alphabetical order 
+// Use a for loop to print each type of scientist in the array
+// use the for loop to find the name with most letters 
+//use the for loop to reverst the order in the array 
