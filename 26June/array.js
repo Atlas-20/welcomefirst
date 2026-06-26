4c3173567eb155cb7dfe2ac07b0b4b4979ff60c2
Atlas <atlastoo012@gmail.com>
2026-06-27 00:07:09 +0530

    done some array

---
+++ 26June/array.js
@@ -0,0 +1,31 @@
+// const arr = [ "apple", "pomegrannate", "grape", "tomato"]
+
+//creating an empty array and providing the elements later;
+
+  const fruits = [];
+   
+    fruits[1] = "pomegranate";
+    fruits[2] = "tomato" ;
+    fruits[3] = " guava";
+    fruits[5] = "mango ";
+    fruits[0] = "null";
+  
+    console.log(fruits);
+      
+    // here even the null is a fruit 
+    fruits[4] = null;
+     console.log(fruits);
+
+     // Now this null is itself is a null not a array named null 
+      
+
+ // creating array with the word "new ". ..
+    
+   const cars = new Array("bmw", "mercedez", "farrari", "rolles-royals");
+   console.log(cars);
+       //it is not such used more even the above is mostly same as it 
+
+  // changing an array element
+    cars[3] = "tata";
+    cars[5] = "Mahindra";
+    console.log(cars);
