36fe8945b49d6a1c0797ac748808b4ef272d4ea7
Atlas <atlastoo012@gmail.com>
2026-06-26 21:20:29 +0530

    let have some

---
+++ 25June/Untitled-1.js
@@ -0,0 +1,13 @@
+// const array1 = [5, 12, 8, 130, 44];
+
+// const isLargeNumber = (element) => element > 13;
+
+// console.log(array1.findIndex(isLargeNumber));
+// // expected output: 3
+ let person = {
+    age: 22,
+    gender: "male",
+    matter: "hallabol",
+
+}
+console.log(person.gender) 
