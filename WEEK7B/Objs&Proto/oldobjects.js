95468a11959682d69e6976e1acce5395ad95452f
Atlas <atlastoo012@gmail.com>
2026-07-08 17:27:53 +0530

    well hello

--- WEEK7B/Objs&Proto/oldobjects.js
+++ WEEK7B/Objs&Proto/oldobjects.js
@@ -15,6 +15,8 @@ const obj2 = {                        //So here in obj2 the getfullname is not t
    
 }
 obj2.__proto__= obj1;  //here we injected the obj1 proto in obj2 proto now getfullname will work in obj2 also without defining in obj2 . Let's see 
+                    //  $$$$$ Its the same thing is done underhood of classes +++++++$$$$TO avoid the playing with proto whihc is
+                    // not consider good that's why the classed where introduced noting different or special$$$$
 
 // obj2.__proto__.__proto__ = null;
 
@@ -22,7 +24,7 @@ console.log(obj1.getfullname());//difference bw console.log(obj1.getfullname)vs 
                                // i means cllingthe function vs just printing the value 
 console.log(obj2.getfullname()); 
 // obj2.push("hello");
-console.log(obj2);
+console.log(obj2);   
 
 //Even if we do the same funtion in the obj2 still we are failed bacaused we are 
 // violatiing a single rule of code DRY(do not repeat ) 
